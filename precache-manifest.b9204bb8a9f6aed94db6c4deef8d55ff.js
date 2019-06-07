e44730ba92c57c01645b67794dd8fa56e9289c49
Sebastian Vidal <sebastian@aytech.ca>
2019-06-06 17:57:29 -0700

    Updates

---
+++ precache-manifest.b9204bb8a9f6aed94db6c4deef8d55ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3f024315f24caa30731",
+    "url": "/webcomic-gh-pages/static/css/main.9ba85fac.chunk.css"
+  },
+  {
+    "revision": "a3f024315f24caa30731",
+    "url": "/webcomic-gh-pages/static/js/main.d45c34ae.chunk.js"
+  },
+  {
+    "revision": "2ee1f6dd13032153667d",
+    "url": "/webcomic-gh-pages/static/js/runtime~main.6875f7c8.js"
+  },
+  {
+    "revision": "841f44c1357a33a12a16",
+    "url": "/webcomic-gh-pages/static/js/2.b1c01b79.chunk.js"
+  },
+  {
+    "revision": "10988712f68b90841e480c01d3752a17",
+    "url": "/webcomic-gh-pages/index.html"
+  }
+];
