d56debfe548731d85f18837ef4d60c8ecc6abc74
zseib <sebastian@aytech.ca>
2019-06-08 19:27:12 -0700

    fixed page 3

--- src/comics/1.js
+++ src/comics/1.js
@@ -6,6 +6,6 @@ export const comics = {
     image: "https://66.media.tumblr.com/ae7b23fe51548c9cf96064b4608a610f/tumblr_pst7gaJDdE1qk80y3o1_1280.png "
   },
   3: {
-    image: "https://66.media.tumblr.com/ae7b23fe51548c9cf96064b4608a610f/tumblr_pst7gaJDdE1qk80y3o1_1280.png"
+    image: "https://66.media.tumblr.com/56bdbe7f83ceb8c2f820b3d12217580b/tumblr_pst7iar4bk1qk80y3o1_1280.png"
   }
  }
