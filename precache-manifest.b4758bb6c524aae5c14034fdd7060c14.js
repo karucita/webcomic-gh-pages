fadba1d0cd736e4a3f4d4fe9eaf92c8a141fd8af
zseib <sebastian@aytech.ca>
2019-06-08 19:43:39 -0700

    Updates

---
+++ precache-manifest.b4758bb6c524aae5c14034fdd7060c14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "82cbe15d40bce172304f",
+    "url": "/static/js/main.c64a6949.chunk.js"
+  },
+  {
+    "revision": "c9563207139f17e8237c",
+    "url": "/static/js/2.2aabaca5.chunk.js"
+  },
+  {
+    "revision": "82cbe15d40bce172304f",
+    "url": "/static/css/main.eafc376b.chunk.css"
+  },
+  {
+    "revision": "70557a27c1e3afc21c4268e74f3096fd",
+    "url": "/index.html"
+  }
+];
